49c40a7d0d9fe9579ea6fb47cc84df225d3d3927
Alexander <alexandermagichi@gmail.com>
2025-03-04 22:16:16 +0200

    feat UserServiceImpl and UserService

---
+++ storecrm/src/main/java/com/storecrm/storecrm/service/userservice/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package com.storecrm.storecrm.service.userservice;
+import com.storecrm.storecrm.model.user.User;
+import com.storecrm.storecrm.repository.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Implementation of the UserService interface.
+ * This class provides the actual logic for user management.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        if (userRepository.existsById(id)) {
+            user.setId(id);
+            return userRepository.save(user);
+        }
+        return null; // Or throw some exception indicating user not found
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
